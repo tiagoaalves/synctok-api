f679d50c370443a476866c72c88e9d07ad9d5f5d
Tiago Alves <tiagolvs22@gmail.com>
2024-04-09 20:37:02 +0100

    Initial commit

---
+++ src/main/java/com/synctok/synctokapi/SynctokApiApplication.java
@@ -0,0 +1,13 @@
+package com.synctok.synctokapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynctokApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynctokApiApplication.class, args);
+	}
+
+}
